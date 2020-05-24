b4b10929b04d9c78f3e08f8e28f200ae46c39ee8
Hackwithvyshu <64751167+keralahacker@users.noreply.github.com>
2020-05-24 08:16:14 +0000

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -4,7 +4,7 @@ echo -e $'\e[31mN.`````````yNNd-```````.N  \e[1;32m Tik-Tok OTP BYPASS PHISHING 
 echo -e $'\e[31mm``````````yNNNNyo-`````m'
 echo -e $'\e[31mm````.-/+:`yNNhhdmo`````m'
 echo -e $'\e[31mm```+hNNmy.yNN+`...`````m'
-echo -e $'\e[31mm``+mNd+:-`yNN+`````````m \e[1;32m Follow on Github :-  \e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
+echo -e $'\e[31mm``+mNd+:-`yNN+`````````m \e[1;32m Follow on Github :- https://github.com/keralahacker/tiktok-h3k.git\e[0m\e[1;33m] \e[0m'
 echo -e $'\e[31mm``sNNy```.hNN+`````````m'
 echo -e $'\e[31mN``.hNNdyydNNy-`````````m'
 echo -e $'\e[31mN:```/ydddhs/.`````````-N     +-+-+-+-+ +-+-+-+-+ +-+-+-+-+-+'
@@ -14,9 +14,6 @@ echo -e $'\e[31mMMNho///////////////ohNMM     +-+-+-+-+ +-+-+-+-+ +-+-+-+-+-+'
 
 
 
-#echo -e $'\e[37m|\e[96mH\e[37m|\e[96ma\e[37m|\e[96mc\e[37m|\e[96mk\e[37m| 
-#\e[37m|\e[96mw\e[37m|\e[96mi\e[37m|\e[96mt\e[37m|\e[96mh\e[37m| 
-#\e[37m|\e[96mV\e[37m|\e[96my\e[37m|\e[96ms\e[37m|\e[96mh\e[37m|\e[96mu'
 
 
 
